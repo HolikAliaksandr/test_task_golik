f7c8f7bae25f4d13f57be8dd06853075798adeb6
HolikAliaksandr <9292770@gmail.com>
2024-09-17 23:28:03 +0300

    ubdate 2

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture(scope="session")
 def browser_context_args(request):
     with sync_playwright() as playwright:
-        #browser = playwright.chromium.launch(headless=False, args=['--start-maximized'])
-        browser = playwright.ChromeBrowser.launch(headless=False, args=['--start-maximized'])
+        browser = playwright.chromium.launch(headless=False, args=['--start-maximized'])
         context = browser.new_context(no_viewport=True)
         context.tracing.start(screenshots=True, snapshots=True, sources=True)
         page = context.new_page()
